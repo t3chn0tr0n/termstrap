c8f83f0667756da835f6b36bf465aea0cc8cdf85
t3chn0tr0n <technotron.avik@gmail.com>
2019-08-07 22:59:32 +0530

    add files to publish at pypi

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    name='termstrap',
+    version='1.2.0',
+    description='Terminal Bootstrap - A Python css file for unicode text formatting in python console output',
+    author='t3chn0tr0n',
+    author_email='technotron.avik@gmail.com',
+    url='https://github.com/t3chn0tr0n/termstrap',
+    py_modules=["css"],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+    ],
+    python_requires='>=3.*.*',
+    project_urls={
+        'Source': 'https://github.com/t3chn0tr0n/termstrap',
+    }
+)
